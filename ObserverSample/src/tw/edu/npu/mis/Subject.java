84dfb2d0e37bd10753b685f978f97a24be14cbc7
d1101410005 <d1101410005@gmail.com>
2015-05-24 15:44:06 +0800

    Add views

--- ObserverSample/src/tw/edu/npu/mis/Subject.java
+++ ObserverSample/src/tw/edu/npu/mis/Subject.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Samael Wang <freesamael@gmail.com>
+ * Copyright (c) 2015, STP
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,41 +29,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Simulate Window objects in GUI toolkits.
  *
- * @author Samael Wang <freesamael@gmail.com>
+ * @author STP
  */
-public class Window {
-
-    private Controller mController;
-    private List<View> mInvalidViews;
-
-    /**
-     * Start the event loop.
-     *
-     * @param c The controller.
-     * @param views The views to draw on the first loop.
-     */
-    public void startEventLoop(Controller c, List<View> views) {
-        mController = c;
-        mInvalidViews = new ArrayList<>(views);
-
-        // Simulate how an event loop works.
-        while (true) {
-            mController.readInput();
-            for (View v : mInvalidViews) {
-                v.onDraw();
-            }
-           // mInvalidViews.clear();
+public class Subject {
+    private List<Observer> mObservers = new ArrayList<>();
+    
+    public void attach(Observer o) {
+        mObservers.add(o);
+    }
+    public void detach(Observer o) {
+        mObservers.remove(o);
+    }
+    public void notifyObserver() {
+        for (Observer o : mObservers){
+            o.update();
         }
     }
-
-    /**
-     * Add a view to a queue for redraw on screen later.
-     *
-     * @param v View to redraw.
-     */
-    public void schduleRedraw(View v) {
-        mInvalidViews.add(v);
-    }
+    
 }
